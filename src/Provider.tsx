6efc8fed9c007a2ca92021a7dbb3cdb97f6ad562
Jean-Charles Bailliez <jc.bailliez@gmail.com>
2021-10-07 15:38:06 +0200

    Check deepEquality when options are set

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -1,4 +1,5 @@
-import React, { useMemo, ReactElement } from 'react'
+import { dequal } from 'dequal'
+import React, { ReactElement, useRef, useState, useEffect } from 'react'
 import useSSR from 'use-ssr'
 import FetchContext from './FetchContext'
 import { FetchContextTypes, FetchProviderProps } from './types'
@@ -11,14 +12,33 @@ export const Provider = ({
 }: FetchProviderProps): ReactElement => {
   const { isBrowser } = useSSR()
 
-  const defaults = useMemo(
-    (): FetchContextTypes => ({
+  const optionsRef = useRef<FetchContextTypes['options']>()
+
+  const [defaults, setDefaults] = useState<FetchContextTypes>(
+    {
       url: url || (isBrowser ? window.location.origin : ''),
       options: options || {},
       graphql // TODO: this will make it so useFetch(QUERY || MUTATION) will work
-    }),
-    [options, graphql, isBrowser, url]
+    }
   )
+
+  useEffect(() => {
+    if (!dequal(optionsRef.current, options)) {
+      optionsRef.current = options
+      setDefaults((current) => ({
+        ...current,
+        options: options || {}
+      }))
+    }
+  }, [options])
+
+  useEffect(() => {
+    setDefaults(current => ({ ...current, url: url || (isBrowser ? window.location.origin : '') }))
+  }, [url, isBrowser])
+
+  useEffect(() => {
+    setDefaults(current => ({ ...current, graphql }))
+  }, [graphql])
 
   return (
     <FetchContext.Provider value={defaults}>{children}</FetchContext.Provider>
